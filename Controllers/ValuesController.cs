7e4e344cde396fcd0ee9471128a624ca8589928e
Pavel Kochkin <kach93@mail.ru>
2019-04-04 15:00:57 +0300

    soon

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiLesson.Controllers
@@ -11,18 +9,46 @@ namespace ApiLesson.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET: api/values
+        //GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] {"this", "is", "get", "method"};
+            return new string[] { "this", "is", "get", "method" };
         }
 
         //GET: api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id , string query)
         {
-            return $"id = {id}";
+            return Ok(new Value() {Id = id, Name = "Name" + id});
         }
+
+        //POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Value value)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            return CreatedAtAction("Get", new {id = value.Id}, value);
+        }
+
+        //PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        //DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Value
+    {
+        public int Id { get; set; }
+        [MinLength(3)]
+        public string Name { get; set; }
     }
 }
