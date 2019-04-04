7a59ca331762a5eea462704977e8f1c04d6f8568
Pavel Kochkin <kach93@mail.ru>
2019-04-04 13:50:36 +0300

    initial

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLesson.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {"this", "is", "get", "method"};
+        }
+
+        //GET: api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"id = {id}";
+        }
+    }
+}
